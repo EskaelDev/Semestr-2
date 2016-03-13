cd6e51721a97dfb15fd135b4e9acf670f715d9cb
Tomek Demczuk <tomekkomek@gmail.com>
2016-03-13 19:21:29 +0100

    dalej nie działa na stingach, jakis nowy błąd, w codeblock dziala

--- Struktury S_2 G_2 Sek_15/Struktury S_2 G_2 Sek_15.cpp
+++ Struktury S_2 G_2 Sek_15/Struktury S_2 G_2 Sek_15.cpp
@@ -8,7 +8,7 @@
 #include <string>
 #include <fstream>
 
-
+/*
 using namespace std;
 
 
@@ -21,11 +21,11 @@ struct ryby
 
 int main()
 {
-	/*string wyraz;
+	//string wyraz;
 	cout << "Podaj tekst: ";
 	cin >> wyraz;
 	cout << "Wprowadziles: \"" << wyraz << "\"" << endl;
-	cout << "D³ugosc wyrazu: " << wyraz.length() << endl;*/
+	//cout << "D³ugosc wyrazu: " << wyraz.length() << endl;
 
 
 	ifstream plik2("S:\plik.txt");
@@ -56,7 +56,7 @@ int main()
 			fscanf(plik, "%f", &tab[i++].polow);
 		}
 	
-	/*FILE *plik;
+	//FILE *plik;
 	if ((plik = fopen("S:\plik.txt", "r")) == NULL)
 		cout << "nie otwarto pliku" << endl;
 	else
@@ -70,7 +70,7 @@ int main()
 		{
 			getline (plik, tab[i].gatunek );
 			fscanf(plik, "%f", &tab[i++].polow);
-		}*/
+		//}
 
 
 		//sumwanie tych samych//
@@ -105,5 +105,71 @@ int main()
 	}
 	_getch();
     return 0;
-}
+}*/
+#include <iostream>
+#include <stdio.h>
+#include <conio.h>
+#include <string>
 
+
+using namespace std;
+
+
+struct ryby
+{
+	string gatunek;
+	float polow;
+};
+
+
+int main()
+{
+	FILE *plik;
+	if ((plik = fopen("S:\plik.txt", "r")) == NULL)
+		cout << "nie otwarto pliku" << endl;
+	else
+	{
+		bool pom;
+		int i = 0, lp = 1;
+		float polowy = 0;
+		ryby *tab = new ryby[100];
+
+		while (!feof(plik))
+		{
+			fscanf(plik, "%s", tab[i].gatunek.c_str());
+			fscanf(plik, "%f", &tab[i++].polow);
+		}
+
+
+		//sumwanie tych samych/////
+		for (int j = 0; j < i; j++)
+			for (int k = j + 1; k < i; k++)
+				if (tab[j].gatunek.compare(tab[k].gatunek))
+					tab[j].polow += tab[k].polow;
+
+
+		for (int j = 0; j < i; j++)
+		{
+			pom = true;
+			if (j > 0)
+			{
+				for (int k = 0; k < j; k++)
+				{
+					if (tab[j].gatunek.compare(tab[k].gatunek))
+						pom = false;
+				}
+				if (pom)
+				{
+					cout << lp + 1 << " " << tab[j].gatunek << " " << tab[j].polow << endl;
+					lp++;
+				}
+			}
+			else cout << lp << " " << tab[j].gatunek << " " << tab[j].polow << endl;
+		}
+
+		delete tab;
+		fclose(plik);
+	}
+	_getch();
+	return 0;
+}
