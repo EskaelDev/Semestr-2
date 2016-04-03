b8fbe9ae73eeb9992686f0096803e3fa2f23e5fc
Tomek Demczuk <tomekkomek@gmail.com>
2016-04-03 13:30:00 +0200

    Nie skonczone

--- Zawieranie Numer_telefonu/Zawieranie Numer_telefonu.cpp
+++ Zawieranie Numer_telefonu/Zawieranie Numer_telefonu.cpp
@@ -12,33 +12,52 @@ enum TypNumeru
 	home,
 	job
 };
+
 class NumerTelefonu
 {
-	TypNumeru typ;
 	unsigned short int NumerKierunkowy;
-	string Numer;
+	void ZerowanieNumeru(char*);
+	char Numer[10];
 
 public:
-	NumerTelefonu(TypNumeru, unsigned short int, string);
+	TypNumeru Typ;
+	NumerTelefonu(TypNumeru, unsigned short int, char*);
 	int WypiszKierunkowy();
-	string WypiszNumer();
+	char* WypiszNumer();
 };
+
+void NumerTelefonu::ZerowanieNumeru(char* tab)
+{
+	for (int i = 0; i < 10; i++)
+		tab[i] = '\0';
+}
+
+NumerTelefonu::NumerTelefonu(TypNumeru typ, unsigned short numerkierunkowy, char* tab)
+{
+	ZerowanieNumeru(Numer);
+	Typ = typ;
+	NumerKierunkowy = numerkierunkowy;
+	strcat(Numer, tab);
+}
 
 int NumerTelefonu::WypiszKierunkowy()
 {
 	return NumerKierunkowy;
 }
 
-string NumerTelefonu::WypiszNumer()
+char* NumerTelefonu::WypiszNumer()
 {
 	return Numer;
 }
-
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////
 class Kontakt
 {
-	string Imie, Nazwisko, Numer1, Numer2, Numer3, Numer4, Numer5;
+	char Imie[10], Nazwisko[20], Numer[1][7];
+	void ZerowanieNumerow();
 public:
-	Kontakt(string, string);
+	Kontakt(char*, char*, char*, char*, char*, char*, char*);
+	void ScalImieNazwisko(char*);
+	NumerTelefonu tabnr[5];
 
 };
 
