1f70590631ae313e5ae69862104243634c43a7ec
Tomek Demczuk <tomekkomek@gmail.com>
2016-04-11 18:23:38 +0200

    Dziala bez realloca

--- Konstruktor kopiujacy/DynArray.cpp
+++ Konstruktor kopiujacy/DynArray.cpp
@@ -6,11 +6,15 @@ using namespace std;
 DynArray::DynArray()
 {
 	rozmiar = 0;
+	tab = (int*)malloc(sizeof(int)*rozmiar);
+
 }
 
 DynArray::DynArray(int N, int M)
 {
 	rozmiar = N;
+
+	tab = (int*)malloc(sizeof(int)*rozmiar);
 
 	for (int i = 0; i < N;i++)
 	{
@@ -18,10 +22,10 @@ DynArray::DynArray(int N, int M)
 	}
 }
 
-DynArray::DynArray(const DynArray &array)
+DynArray::DynArray(const DynArray& array)
 {
 	rozmiar = array.rozmiar;
-	tab = new int[rozmiar];
+	tab = (int*)malloc(sizeof(int)*rozmiar);
 
 	for (int i = 0; i < rozmiar;i++)
 	{
@@ -37,20 +41,44 @@ int DynArray::size()
 
 void DynArray::display()
 {
-	for (int i = 0; i < rozmiar;i++)
+	cout << "[";
+	for (int i = 0; i < rozmiar; i++)
 	{
-		cout<<
+		cout << tab[i];
+		if (i < rozmiar - 1)
+			cout << ", ";
 	}
+	cout << "]";
+
 }
 
-void DynArray::add(int i)
+void DynArray::add(int n)
 {
-	if (rozmiar>0)
-	{
-		int *tmp = new int[rozmiar];
+	
 
-	}
+		int *tmp = (int*)malloc(sizeof(int)*rozmiar);
+
+		for (int i = 0; i < rozmiar; i++)
+			tmp[i] = tab[i];
+
+		rozmiar++;
+
+		free(tab);
+		tab = (int*)malloc(sizeof(int)*rozmiar);
+
+		for (int i = 0; i < rozmiar; i++)
+			tab[i] = tmp[i];
+		tab[rozmiar-1] = n;
+		free(tmp);
 }
+
+/*
+void DynArray::add(int l)
+{
+	tab = (int*)realloc(tab, ++rozmiar * sizeof(int));
+	tab[rozmiar - 1] = l;
+}
+*/
 
 int DynArray::get(int i)
 {
--- Konstruktor kopiujacy/Konstruktor kopiujacy.cpp
+++ Konstruktor kopiujacy/Konstruktor kopiujacy.cpp
@@ -5,37 +5,56 @@
 #include <iostream>
 #include "DynArray.h"
 #include <conio.h>
+#include <Windows.h>
+
+void portableSleep(int sec) {
+#   ifdef POSIX
+	sleep(sec);
+#   endif
+#   ifdef WINDOWS
+	Sleep(sec * 1000);
+#   endif
+}
+
 
 using namespace std;
 
-
+/*
 int main()
 {
 	const int tst_size1 = 20;
-	const int tst_size2 = 50000;
+	const int tst_size2 = 5000;
 
 	DynArray array1;
 	printf("array1 size: %d\n", array1.size());
-	printf("array1 disp: ");
+	printf("array1 disp: [");
 	array1.display();
+	printf("]");
+
 
 	DynArray array2;
 	printf("array2 size: %d\n", array2.size());
-	printf("array2 disp: ");
+	printf("array1 disp: [");
 	array1.display();
+	printf("]");
+
 
 	for (int i = 0; i < tst_size1; ++i)
 	{
 		array1.add(1);
 	}
 	printf("\narray1 size: %d\n", array1.size());
-	printf("array1 disp: ");
+	printf("array1 disp: [");
 	array1.display();
+	printf("]");
+	
 
 	DynArray array3(array1);
 	printf("array3 size: %d\n", array3.size());
-	printf("array3 disp: ");
+	printf("array1 disp: [");
 	array3.display();
+	printf("]");
+
 
 	printf("\n\nget test : ");
 	for (int i = 0; i < array3.size(); ++i)
@@ -60,12 +79,63 @@ int main()
 	delete arrayX;
 
 	printf("\n\narrayY size: %d\n", arrayY.size());
-	printf("arrayY disp: ");
+	printf("array1 disp: [");
 	arrayY.display();
+	printf("]");
 
 	printf("\nKoniec.");
 	_getch();
 
     return 0;
-}
+}*/
 
+int main()
+{
+const int tst_size1 = 20;
+const int tst_size2 = 50000;
+
+DynArray array1;
+printf("array1 size: %d\n", array1.size());
+printf("array1 disp: ");
+array1.display();
+
+DynArray array2(10, 2);
+printf("\n\narray2 size: %d\n", array2.size());
+printf("array2 disp: ");
+array2.display();
+
+for (int i = 0; i < tst_size1; i++)
+	array1.add(i);
+printf("\n\narray1 size: %d\n", array1.size());
+printf("array1 disp: ");
+array1.display();
+
+DynArray array3(array1);
+printf("\n\narray3 size: %d\n", array3.size());
+printf("array3 disp: ");
+array3.display();
+
+printf("\n\nget test    : ");
+for (int i = 0; i < array3.size(); i++)
+	printf("%d ", array3.get(i));
+
+for (int i = 0; i < tst_size2; i++)
+	array1.add(i);
+/**/
+printf("\n\n");
+for (int i = array1.size() - 5; i < array1.size(); i++)
+	printf("array1[%d] = %d\n", i, array1.get(i));
+DynArray * arrayX = new DynArray(10, 1);
+arrayX->add(1000);
+DynArray arrayY(*arrayX);
+delete arrayX;
+
+printf("\n\narraY size: %d\n", arrayY.size());
+printf("arrayY disp: ");
+arrayY.display();
+
+printf("\nKoniec.");
+//portableSleep(2);
+getch();
+return 0;
+}
